86c553a7d532b1377bb00d6ecf5c3e78af1252d4
Loïc Prosi <loicprosi@gmail.com>
2023-08-30 19:14:11 +0200

    feat: added card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Card = ({ id, title, image }) => {
+  return (
+    <Link className="card" to={`/rentals/${id}`}>
+      <img className="card__image" src={image} alt={title} />
+      <div className="card__title-container">
+        <h2 className="card__title">{title}</h2>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
