3ce7bcd1c080df5bf8fd7738557f7170227fc3e5
Loïc Prosi <loicprosi@gmail.com>
2023-08-30 05:11:45 +0200

    feat: added routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import "./index.sass";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Housing from "./pages/Housing";
+import About from "./pages/About";
+import Error from "./pages/Error";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/housing",
+        element: <Housing />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "*",
+        element: <Error />
+      }
+    ]
+  }
+]);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
