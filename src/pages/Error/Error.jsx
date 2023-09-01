0ba586c13bad8955e3becba3dd23a005a8f3a8ee
Loïc Prosi <loicprosi@gmail.com>
2023-09-01 22:39:50 +0200

    feat: added error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <h1>404</h1>;
+  return (
+    <div className="error-page">
+      <h2 className="error-page__title">404</h2>
+      <h3 className="error-page__subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </h3>
+      <Link className="error-page__return-link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
