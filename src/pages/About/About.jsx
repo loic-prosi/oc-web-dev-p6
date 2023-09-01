23c717462da653300a3e7407e0b64f63b4db35d9
Loïc Prosi <loicprosi@gmail.com>
2023-09-01 21:41:41 +0200

    feat: added collapse component

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../components/Banner";
+import Collapse from "../../components/Collapse";
 import mountain from "../../assets/images/mountain.png";
 
 const About = () => {
@@ -9,6 +10,24 @@ const About = () => {
         image={mountain}
         imageAlt="Paysage de montagnes enneigées avec une forêt de sapins"
       />
+      <section className="collapses">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </section>
     </>
   );
 };
