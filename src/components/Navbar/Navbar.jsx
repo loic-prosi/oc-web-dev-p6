909871fa28f4425b1943d076fcf756ba62be23ad
Loïc Prosi <loicprosi@gmail.com>
2023-09-07 10:07:37 +0200

    refactor: navbar component html elements semantic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,45 +3,43 @@ import logo from "../../assets/images/logo.svg";
 
 const Navbar = () => {
   return (
-    <header className="navbar">
+    <nav className="navbar">
       <h1 className="navbar__title">
         <Link to="/">
           <img className="navbar__logo" src={logo} alt="Kasa" />
         </Link>
       </h1>
-      <nav>
-        <ul className="navbar__links">
-          <li>
-            <NavLink
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? ""
-                  : isActive
-                  ? "navbar__link navbar__link--active"
-                  : "navbar__link"
-              }
-              to="/"
-            >
-              Accueil
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? ""
-                  : isActive
-                  ? "navbar__link navbar__link--active"
-                  : "navbar__link"
-              }
-              to="/about"
-            >
-              A Propos
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <ul className="navbar__links">
+        <li>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? ""
+                : isActive
+                ? "navbar__link navbar__link--active"
+                : "navbar__link"
+            }
+            to="/"
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? ""
+                : isActive
+                ? "navbar__link navbar__link--active"
+                : "navbar__link"
+            }
+            to="/about"
+          >
+            A Propos
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
