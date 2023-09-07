59aa83c15d9174298d3e3d8e3daeab9fe19cd3c6
Loïc Prosi <loicprosi@gmail.com>
2023-09-07 10:06:36 +0200

    chore: added tests configuration file

---
+++ src/utils/testsConfig.js
@@ -0,0 +1,37 @@
+import { render } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+
+import routesConfig from "./routesConfig";
+
+const renderPage = (page, rentals) => {
+  const routes = routesConfig(rentals ? rentals : []);
+
+  let router;
+  switch (page) {
+    case "home":
+      router = createMemoryRouter(routes);
+      break;
+    case "rentals":
+      router = createMemoryRouter(routes, {
+        initialEntries: ["/rentals/:id"]
+      });
+      break;
+    case "about":
+      router = createMemoryRouter(routes, {
+        initialEntries: ["/about"]
+      });
+      break;
+    case "error":
+      router = createMemoryRouter(routes, {
+        initialEntries: ["/error"]
+      });
+      break;
+    default:
+      router = createMemoryRouter(routes);
+      break;
+  }
+
+  render(<RouterProvider router={router} />);
+};
+
+export default renderPage;
